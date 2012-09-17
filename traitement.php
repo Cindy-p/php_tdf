0a59698e48b39f7f331342226d7c2cbe478928d9
Steven <bankai14130@gmail.com>
2012-09-17 15:14:23 +0200

    maj_traitement

--- traitement.php
+++ traitement.php
@@ -7,12 +7,14 @@ include("formulaire.php");
     echo "</pre>";
 }*/
 
-$db_username = "cindy.perat";
-$db_password = "Peratlccsl61";
+$db_username = "steven";
+$db_password = "Nokia5530";
 $db = "oci:dbname=xe";
 $conn = new PDO($db, $db_username, $db_password);
 
-// traitement des données entrées -------------------------------
+// traitement des données entrées -----------------------------------------------------------------------------------------------------------------------------
+
+//--------------- NOM COUREUR ---------------
 
 if(isset($_POST['nomCoureur'])){
 	$_POST['nomCoureur'] = htmlspecialchars($_POST['nomCoureur']);
@@ -30,6 +32,8 @@ if(isset($_POST['nomCoureur'])){
 	echo$_POST['nomCoureur']."<br />";
 }
 
+//--------------- PRENOM COUREUR ---------------
+
 if(isset($_POST['prenomCoureur'])){
 	$_POST['prenomCoureur'] = htmlspecialchars($_POST['prenomCoureur']);
 	echo $_POST['prenomCoureur']."<br />";
@@ -40,21 +44,18 @@ if(isset($_POST['prenomCoureur'])){
 		exit;
 	}
 	
-	$_POST['prenomCoureur'] = traitementAccents($_POST['prenomCoureur']);
-	$_POST['prenomCoureur'] = ucfirst($_POST['prenomCoureur']);
+	$_POST['prenomCoureur'] = traitementAccentsP($_POST['prenomCoureur']);
+	$_POST['prenomCoureur'] = ucfirst(strtolower($_POST['prenomCoureur']));
 	
 	echo$_POST['prenomCoureur']."<br />";
 }
 
+//--------------- FONCTIONS DE TRAITEMENT ---------------
+
 function traitementAccents($chaine){
-	if($chaine == $_POST['nomCoureur']){
-		if(preg_match("#[àÀáÁâÂãÃäÄåÅ]#", $chaine))
-			$chaine = preg_replace("#[àÀáÁâÂãÃäÄåÅ]#", "a", $chaine);
-	}	
-	else {
-		if(preg_match("#[ÀáÁÂãÃäÄåÅ]#", $chaine))
-			$chaine = preg_replace("#[ÀáÁÂãÃäÄåÅ]#", "a", $chaine);
-	}
+	
+	if(preg_match("#[àÀáÁâÂãÃäÄåÅ]#", $chaine))
+		$chaine = preg_replace("#[àÀáÁâÂãÃäÄåÅ]#", "a", $chaine);
 		
 	if(preg_match("#[æÆ]#", $chaine))
 		$chaine = preg_replace("#[æÆ]#", "ae", $chaine);
@@ -62,54 +63,93 @@ function traitementAccents($chaine){
 	if(preg_match("#[èÈéÉêÊëË]#", $chaine))
 		$chaine = preg_replace("#[èÈéÉêÊëË]#", "e", $chaine);
 	
-	if($chaine == $_POST['nomCoureur']){
-		if(preg_match("#[ìÌíÍîÎïÏ]#", $chaine))
-			$chaine = preg_replace("#[ìÌíÍîÎïÏ]#", "i", $chaine);
-	}
-	else {
-		if(preg_match("#[ìÌíÍÎÏ]#", $chaine))
-			$chaine = preg_replace("#[ìÌíÍÎÏ]#", "i", $chaine);
-	}
-	
-	if($chaine == $_POST['nomCoureur']){	
-		if(preg_match("#[òÒóÓôÔõÕöÖøØ]#", $chaine))
-			$chaine = preg_replace("#[òÒóÓôÔõÕöÖøØ]#", "o", $chaine);
-	}
-	else{	
-		if(preg_match("#[òÒóÓÔõÕöÖøØ]#", $chaine))
-			$chaine = preg_replace("#[òÒóÓÔõÕöÖøØ]#", "o", $chaine);
-	}
-	
-	if($chaine == $_POST['nomCoureur']){	
-		if(preg_match("#[ùÙúÚûÛüÜ]#", $chaine))
-			$chaine = preg_replace("#[ùÙúÚûÛüÜ]#", "u", $chaine);
-	}
-	else {	
-		if(preg_match("#[ÙúÚÛÜ]#", $chaine))
-			$chaine = preg_replace("#[ÙúÚÛÜ]#", "u", $chaine);
-	}
+	if(preg_match("#[ìÌíÍîÎïÏ]#", $chaine))
+		$chaine = preg_replace("#[ìÌíÍîÎïÏ]#", "i", $chaine);
+		
+	if(preg_match("#[òÒóÓôÔõÕöÖøØ]#", $chaine))
+		$chaine = preg_replace("#[òÒóÓôÔõÕöÖøØ]#", "o", $chaine);
+
+	if(preg_match("#[ùÙúÚûÛüÜ]#", $chaine))
+		$chaine = preg_replace("#[ùÙúÚûÛüÜ]#", "u", $chaine);
 	
 	if(preg_match("#[ñÑ]#", $chaine))
-			$chaine = preg_replace("#[ñÑ]#", "n", $chaine);
+		$chaine = preg_replace("#[ñÑ]#", "n", $chaine);
 		
-		if(preg_match("#[çÇ]#", $chaine))
-			$chaine = preg_replace("#[çÇ]#", "c", $chaine);
-	
-	if($chaine == $_POST['nomCoureur']){	
-		if(preg_match("#[ýÝÿŸ]#", $chaine))
-			$chaine = preg_replace("#[ýÝÿŸ]#", "y", $chaine);
-	}
-	else{	
-		if(preg_match("#[ýÝŸ]#", $chaine))
-			$chaine = preg_replace("#[ýÝŸ]#", "y", $chaine);
-	}
+	if(preg_match("#[çÇ]#", $chaine))
+		$chaine = preg_replace("#[çÇ]#", "c", $chaine);
+		
+	if(preg_match("#[ýÝÿŸ]#", $chaine))
+		$chaine = preg_replace("#[ýÝÿŸ]#", "y", $chaine);
 	
 	if(preg_match("#[ß]#", $chaine))
 		$chaine = preg_replace("#[ß]#", "ss", $chaine);
 		
 	return $chaine;
 }
-// requête d'insertion des coureurs -----------------------------
+
+function traitementAccentsP($chaine){
+	
+	if(preg_match("#[ÀáÁÂãÃÄåÅ]#", $chaine)){
+		if(preg_match("#[áÁåÅãÃ]#", $chaine))
+			$chaine = preg_replace("#[áÁåÅãÃ]#", "a", $chaine);
+		if(preg_match("#À#", $chaine))
+			$chaine = preg_replace("#À#", "à", $chaine);
+		if(preg_match("#Â#", $chaine))
+			$chaine = preg_replace("#Â#", "â", $chaine);
+		if(preg_match("#Ä#", $chaine))
+			$chaine = preg_replace("#Ä#", "ä", $chaine);
+	}
+	
+	if(preg_match("#[æÆ]#", $chaine))
+		$chaine = preg_replace("#[æÆ]#", "ae", $chaine);
+		
+	if(preg_match("#[ÈÉÊË]#", $chaine)){
+		if(preg_match("#[È]#", $chaine))
+			$chaine = preg_replace("#[È]#", "è", $chaine);
+		if(preg_match("#[É]#", $chaine))
+			$chaine = preg_replace("#[É]#", "é", $chaine);
+		if(preg_match("#[Ê]#", $chaine))
+			$chaine = preg_replace("#[Ê]#", "ê", $chaine);
+		if(preg_match("#[Ë]#", $chaine))
+			$chaine = preg_replace("#[Ë]#", "ë", $chaine);
+	}
+
+	if(preg_match("#[ìÌíÍÎÏ]#", $chaine)){
+		$chaine = preg_replace("#[ìÌíÍ]#", "i", $chaine);
+		if(preg_match("#[Î]#", $chaine))
+			$chaine = preg_replace("#[Î]#", "î", $chaine);
+		if(preg_match("#[Ï]#", $chaine))
+			$chaine = preg_replace("#[Ï]#", "ï", $chaine);
+	}
+	
+	if(preg_match("#[òÒóÓÔõÕÖøØ]#", $chaine)){
+		$chaine = preg_replace("#[òÒóÓõÕøØ]#", "o", $chaine);
+		if(preg_match("#[Ï]#", $chaine))
+			$chaine = preg_replace("#[Ö]#", "ö", $chaine);
+		if(preg_match("#[Ï]#", $chaine))
+			$chaine = preg_replace("#[Ô]#", "ô", $chaine);
+	}
+
+	if(preg_match("#[ùÙúÚûÛüÜ]#", $chaine)) // à finir
+		$chaine = preg_replace("#[ùÙúÚûÛüÜ]#", "u", $chaine);
+	
+	if(preg_match("#[ñÑ]#", $chaine))
+		$chaine = preg_replace("#[ñÑ]#", "n", $chaine);
+		
+	if(preg_match("#[çÇ]#", $chaine))
+		$chaine = preg_replace("#[çÇ]#", "c", $chaine);
+		
+	if(preg_match("#[ýÝÿŸ]#", $chaine))
+		$chaine = preg_replace("#[ýÝÿŸ]#", "y", $chaine);
+	
+	if(preg_match("#[ß]#", $chaine))
+		$chaine = preg_replace("#[ß]#", "ss", $chaine);
+		
+	return $chaine;
+}
+
+// requête d'insertion des coureurs ---------------------------------------------------------------------------------------------------------------------------
+// NE PAS SUPPRIMER !! 
 
 /*
 $req1 = $conn->query("select max(N_COUREUR)+5 as NUM from tdf_coureur_bidon");
