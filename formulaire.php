921da915c50066df0aba9d30ac4de941718cc50c
Steven <bankai14130@gmail.com>
2012-10-02 13:48:25 +0200

    maj_majeure

--- formulaire.php
+++ formulaire.php
@@ -5,7 +5,7 @@
     </head>
     
     <body>
-        <?php
+		<?php
             function verifRempli($n) {
                 if (isset($_POST[$n])) {
                     $var = $_POST[$n];
@@ -24,105 +24,105 @@
         <div class="container">
             <form class="form-horizontal" action="<?php $_SERVER['PHP_SELF'] ?>" method="post" enctype="application/x-www-form-urlencoded">
                 <legend>Insertion dans la table coureur</legend>
-
-            <!-- NOM ---------------------------------------------------- -->
             
+			<!-- NOM ---------------------------------------------------- -->
+			
                 <?php if($isValidNom){ ?>
                   <div class="control-group">    
                       <label class="control-label" for="nomCoureur">Nom*</label>
                       <div class="controls">
-                          <input type="text" name="nomCoureur" id="nomCoureur" placeholder="Nom" value="<?php verifRempli('nomCoureur'); ?>" maxlength="30" required> <!-- modif -->
+                          <input type="text" name="nomCoureur" id="nomCoureur" maxlength = "30" placeholder="Nom" value="<?php verifRempli('nomCoureur'); ?>" maxlength="30" <!-- required-->> 
                       </div>
                   </div>
                 <?php }else{ ?>
                   <div class="control-group error">
                       <label class="control-label" for="nomCoureur">Nom*</label>
                       <div class="controls">
-                          <input type="text" name="nomCoureur" id="nomCoureur" maxlength="30" required>
+                          <input type="text" name="nomCoureur" id="nomCoureur" maxlength = "30" <!-- required-->>
                           <span class="help-inline">Veuillez entrer un nom valide</span>
                       </div>
                   </div>
                 <?php } ?>
+				
+			<!-- PRENOM ----------------------------------------------------  -->
                 
-            <!-- PRENOM ----------------------------------------------------  -->
-            
                 <?php if($isValidPrenom){ ?>            
                     <div class="control-group">    
                         <label class="control-label" for="prenomCoureur">Prénom*</label>
                         <div class="controls">
-                            <input type="text" name="prenomCoureur" id="prenomCoureur" placeholder="Prénom" value="<?php verifRempli('prenomCoureur'); ?>" maxlength="30" required> <!-- modif -->
+                            <input type="text" name="prenomCoureur" id="prenomCoureur" placeholder="Prénom" value="<?php verifRempli('prenomCoureur'); ?>" maxlength="30" <!-- required-->>
                         </div>
                     </div>
                 <?php }else{ ?>
                   <div class="control-group error">
-                      <label class="control-label" for="prenomCourreur">Prénom</label>
+                      <label class="control-label" for="prenomCourreur">Prénom*</label>
                       <div class="controls">
-                          <input type="text" name="prenomCoureur" id="prenomCoureur" maxlength="30" required>
+                          <input type="text" name="prenomCoureur" id="prenomCoureur" maxlength = "30" <!-- required-->>
                           <span class="help-inline">Veuillez entrer un prénom valide</span>
                       </div>
                   </div>
                 <?php } ?>
-                
-            <!-- ANNEE DE NAISSANCE ----------------------------------------------------  -->
+				
+			<!-- ANNEE DE NAISSANCE ----------------------------------------------------  -->
             
                 <div class="control-group">    
                     <label class="control-label" for="anneeNaissance"> Année de naissance </label>
                     <div class="controls">
-                        <select name="anneeNaissance" id="anneeNaissance" size="1">
-                            <option><?php echo ''; ?></option>
+                        <select name ="anneeNaissance" id ="anneeNaissance" size ="1">
+							<option><?php echo ''; ?></option>
                             <?php for ($i=1900 ; $i<=(date('Y')-17) ; $i++): ?>
-                                <option value="<?php echo $i; ?>" <?php verifSelect('anneeNaissance', $i); ?>> <?php echo $i; ?> </option> <!-- modif -->
-                            <?php endfor; ?>
+                                <option value="<?php echo $i; ?>" <?php verifSelect('anneeNaissance', $i); ?>> <?php echo $i; ?> </option>
+							<?php endfor; ?>
                         </select>
                     </div>
                 </div>
-                
-            <!-- PAYS ----------------------------------------------------  -->
+				
+			<!-- PAYS ----------------------------------------------------  -->
             
                 <div class="control-group">
                     <label class="control-label" for="nomPays"> Pays* </label>
                     <div class="controls">
-                        <select name="nomPays" id="nomPays" size="1" required>
+                        <select name="nomPays" id = "nomPays" size="1" <!-- required-->>
                             <option><?php echo ''; ?></option>
-                            <?php
-                                $conn = new PDO("oci:dbname=xe", "cindy.perat", "Peratlccsl61");
+							<?php
+								$conn = new PDO("oci:dbname = xe", "steven", "Nokia5530");
                                 $req = $conn->query("select NOM from TDF_PAYS order by NOM");
-                                while ($donnees = $req->fetch()) { ?> <!-- modif -->
-                                    <option value="<?php echo $donnees['NOM']; ?>" <?php verifSelect('nomPays', $donnees['NOM']); ?> > <?php echo $donnees['NOM']; ?> </option> <!-- modif -->
-                            <?php } ?> <!-- modif -->
+                                while ($donnees = $req->fetch()) { ?>
+                                    <option value="<?php echo $donnees['NOM']; ?>" <?php verifSelect('nomPays', $donnees['NOM']); ?> > <?php echo $donnees['NOM']; ?> </option>
+                            <?php } ?>
                         </select>
                     </div>
                 </div>
+				
+			<!-- ANNEE TDF ----------------------------------------------------  -->
                 
-            <!-- ANNEE TDF ----------------------------------------------------  -->
-            
                 <div class="control-group">    
                     <label class="control-label" for="anneeTdf">Année de participation</label>
                     <div class="controls">
-                        <select name="anneeTdf" id="anneeTdf" size="1">
+                        <select name="anneeTdf" id = "anneeTdf" size="1">
                             <option><?php echo ''; ?></option>
-                            <?php
-                                $conn = new PDO("oci:dbname=xe", "cindy.perat", "Peratlccsl61");
+							<?php
+								$conn = new PDO("oci:dbname = xe", "steven", "Nokia5530");
                                 $req = $conn->query("select ANNEE from TDF_ANNEE order by ANNEE");
-                                while ($donnees = $req->fetch()) { ?> <!-- modif -->
-                                    <option value="<?php echo $donnees['ANNEE']; ?>" <?php verifSelect('anneeTdf', $donnees['ANNEE']); ?> > <?php echo $donnees['ANNEE']; ?> </option> <!-- modif -->
-                            <?php } ?> <!-- modif -->
+                                while ($donnees = $req->fetch()) { ?>
+                                    <option value="<?php echo $donnees['ANNEE']; ?>" <?php verifSelect('anneeTdf', $donnees['ANNEE']); ?> > <?php echo $donnees['ANNEE']; ?> </option>
+                            <?php } ?>
                         </select>
                     </div>
                 </div>
-                
-            <!-- COMPTE ORACLE ET DATE INSERT ----------------------------------------------------  -->
             
-                <input type="hidden" name="date_insert" value="<?php echo date("d/m/y"); ?>">
-                <input type="hidden" name="compte_oracle" value="cindy.perat">
-            
-            <!-- BOUTON VALIDER ----------------------------------------------------  -->
-            
+			<!-- COMPTE ORACLE ET DATE INSERT ----------------------------------------------------  -->
+			
+				<input type = "hidden" name = "date_insert" value = "<?php echo date("d/m/y"); ?>" >
+				<input type = "hidden" name = "compte_oracle" value = "steven" >
+				
+			<!-- BOUTON VALIDER ----------------------------------------------------  -->	
+				
                 <div class="controls">
                     <button type="submit" class="btn"/>Valider</button>
                 </div>
-                
-                <p> * : champs obligatoire </p>
+				
+				<p> * : champs obligatoire </p>
             </form>
         </div>
     </body>
--- traitement.php
+++ traitement.php
@@ -5,7 +5,7 @@
 //--------------- NOM COUREUR ---------------
 
 if(isset($_POST['nomCoureur'])){
-	$nomCoureur = htmlspecialchars($_POST['nomCoureur']); // on récupère ce qui a été tapé dans la case nomCoureur
+	$nomCoureur = htmlspecialchars($_POST['nomCoureur']);
   
 	if (preg_match("#^['| |-]|[\#\!\^\$\(\)\[\]\{\}\?\+\*\.\\\"%&,/:;@~_\|0-9]|[-]{3,}|[-| ]$#", $nomCoureur)){
         $isValidNom = false;
@@ -13,9 +13,19 @@ if(isset($_POST['nomCoureur'])){
 	else {
         $nomCoureur = traitementAccents($nomCoureur);
         $nomCoureur = strtoupper($nomCoureur);
-        $isValidNom = true;
-        //echo "$nomCoureur <br />";
-    }
+		
+		$nomCoureur = suppr_inutile(" ", $nomCoureur); //modifié
+		$nomCoureur = suppr_inutile("-", $nomCoureur); //modifié
+		$nomCoureur = suppr_inutile("'", $nomCoureur); //modifié
+		
+		if(preg_match("#^[a-zA-Z\' -]{2,}$#", $nomCoureur)){
+			$isValidNom = true;
+		}
+		else{
+			$isValidNom = false;
+		}
+    //  echo $nomCoureur."<br />";
+  }
 }
 
 //--------------- PRENOM COUREUR ---------------
@@ -23,18 +33,38 @@ if(isset($_POST['nomCoureur'])){
 if(isset($_POST['prenomCoureur'])){
 	$prenomCoureur = htmlspecialchars($_POST['prenomCoureur']);
 	
-	if(preg_match("#^['| |-]|[\#\!\^\$\(\)\[\]\{\}\?\+\*\.\\\"%&,/:;@~_\|0-9]|[-]{3,}|[-| ]$#", $prenomCoureur)){
+	if(preg_match("#^[ |-]|[\#\!\^\$\(\)\[\]\{\}\?\+\*\.\\\"%&,/:;@~_\|0-9]|[-]{2,}|[']{2,}|[-| ]$#", $prenomCoureur)){
         $isValidPrenom = false;
 	}
 	else {
-        $prenomCoureur = traitementAccentsP($prenomCoureur);
-        $prenomCoureur = ucfirst(strtolower($prenomCoureur));
-        $isValidPrenom = true;
-        //echo "$prenomCoureur <br />";
+		$prenomCoureur = traitementAccentsP($prenomCoureur);
+		
+		if(preg_match("#^[a-zA-Zàâäéèêëîïôöùûüÿç\' -]{2,}$#", $prenomCoureur)){
+			$isValidPrenom = true;
+		}
+		else{
+			$isValidPrenom = false;
+		}
+	
+	//	echo $prenomCoureur."<br />";
   }
 }
 
 //--------------- FONCTIONS DE TRAITEMENT ---------------
+
+//modif envoyée ---------------------------------------------------------------------------------
+function suppr_inutile($separateur, $chaine){
+	
+	if(preg_match("#( ){1,}$separateur( ){1,}|( ){1,}$separateur|$separateur( ){1,}#", $chaine))
+		$chaine = preg_replace("#( ){1,}$separateur( ){1,}|( ){1,}$separateur|$separateur( ){1,}#", "$separateur", $chaine);
+	
+	else if(preg_match("#('){1,}$separateur('){1,}|('){1,}$separateur|$separateur('){1,}#", $chaine))
+		$chaine += "ß";
+	
+	return $chaine;
+}
+
+//modif envoyée ---------------------------------------------------------------------------------
 
 function traitementAccents($chaine){
 	
@@ -70,12 +100,41 @@ function traitementAccents($chaine){
 		
 	return $chaine;
 }
+// modif à envoyer --------------------------------------------------------------------------------
+
+function prenomCompose($separateur, $chaine){
+		
+		if(preg_match("#$separateur#", $chaine)){
+			
+			$chaine = suppr_inutile($separateur, $chaine);
+			
+			$tab_chaine = explode("$separateur", $chaine);
+			
+			for( $j = 0 ; $j < count($tab_chaine) ; $j++){ 
+				$tab_parties = str_split($tab_chaine[$j]);
+				$tab_parties[0] = traitementAccents($tab_parties[0]);
+				$tab_chaine[$j] = implode("", $tab_parties);
+				$tab_chaine[$j] = ucfirst($tab_chaine[$j]);
+			}
+			
+			$chaine = implode("$separateur", $tab_chaine);
+		}
+	
+	return $chaine;
+}
 
 function traitementAccentsP($chaine){
 	
-	$tab_chaine = str_split($chaine);
-	$tab_chaine[0] = traitementAccents($tab_chaine[0]);
-	$chaine = implode( "", $tab_chaine);
+	$tab_parties = str_split($chaine);
+	$tab_parties[0] = traitementAccents($tab_parties[0]);
+	$chaine = implode( "", $tab_parties);
+	$chaine = ucfirst(strtolower($chaine));
+
+	$chaine = prenomCompose(" ", $chaine); 
+	$chaine = prenomCompose("-", $chaine); 
+	$chaine = prenomCompose("'", $chaine); 	
+
+//modif à envoyer ---------------------------------------------------------------------------------
 	
 	if(preg_match("#[ÀáÁÂãÃÄåÅ]#", $chaine)){
 		if(preg_match("#[áÁåÅãÃ]#", $chaine))
@@ -142,9 +201,9 @@ function traitementAccentsP($chaine){
 	return $chaine;
 }
 
-// Confirmation ---------------------------------------------------------
+//--------------- CONFIRMATION -----------------
 
-if ((isset($nomCoureur) and $isValidNom) and (isset($prenomCoureur) and $isValidPrenom) and
+ if ((isset($nomCoureur) and $isValidNom) and (isset($prenomCoureur) and $isValidPrenom) and
     isset($_POST['nomPays']) and isset($_POST['date_insert']) and isset($_POST['compte_oracle'])) {
     echo "Nom : $nomCoureur <br />";
     echo "Prénom : $prenomCoureur <br />";
